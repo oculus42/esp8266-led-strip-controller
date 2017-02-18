4100dace9b752ec00edbdf8d7ed3440eae514acb
Samuel Rouse <samuel.rouse@gmail.com>
2017-02-17 19:27:40 -0500

    copy settings for sleep setup

---
+++ settings.h
@@ -0,0 +1,19 @@
+/**The MIT License (MIT)
+Based on weather_station_color/settings.h by Daniel Eichhorn
+See more at http://blog.squix.ch
+*/
+
+// Setup
+const int UPDATE_INTERVAL_SECS = 10 * 60;  // Update every 10 minutes
+boolean USE_TOUCHSCREEN_WAKE = true;       // use the touchscreen to wake up, ~90mA current draw
+boolean DEEP_SLEEP = false;                 // use the touchscreen for deep sleep, ~10mA current draw but doesnt work
+int     AWAKE_TIME = 5;                   // how many seconds to stay 'awake' before going back to zzz
+
+// Pins for the ILI9341
+#define TFT_DC 15
+#define TFT_CS 0
+
+// pins for the touchscreen
+#define STMPE_CS 16
+#define STMPE_IRQ 4
+
