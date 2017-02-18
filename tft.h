d49daa71d130a4c46aa3d03d86e2b14c2861edca
Samuel Rouse <samuel.rouse@gmail.com>
2017-02-17 19:27:58 -0500

    Extract tft setup

---
+++ tft.h
@@ -0,0 +1,53 @@
+
+#include <SPI.h>
+#include <Wire.h>      // this is needed even tho we aren't using it
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ILI9341.h> // Hardware-specific library
+#include <Adafruit_STMPE610.h>
+
+#ifdef ESP8266
+   #define STMPE_CS 16
+   #define TFT_CS   0
+   #define TFT_DC   15
+   #define SD_CS    2
+#endif
+#ifdef __AVR_ATmega32U4__
+   #define STMPE_CS 6
+   #define TFT_CS   9
+   #define TFT_DC   10
+   #define SD_CS    5
+#endif
+#ifdef ARDUINO_SAMD_FEATHER_M0
+   #define STMPE_CS 6
+   #define TFT_CS   9
+   #define TFT_DC   10
+   #define SD_CS    5
+#endif
+#ifdef TEENSYDUINO
+   #define TFT_DC   10
+   #define TFT_CS   4
+   #define STMPE_CS 3
+   #define SD_CS    8
+#endif
+#ifdef ARDUINO_STM32_FEATHER
+   #define TFT_DC   PB4
+   #define TFT_CS   PA15
+   #define STMPE_CS PC7
+   #define SD_CS    PC5
+#endif
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+Adafruit_STMPE610 ts = Adafruit_STMPE610(STMPE_CS);
+
+#include "ArialRoundedMTBold_36.h"
+
+// Additional UI functions
+#include "GfxUi.h"
+GfxUi ui = GfxUi(&tft);
+
+// This is calibration data for the raw touch data to the screen coordinates
+#define TS_MINX 3800
+#define TS_MAXX 100
+#define TS_MINY 100
+#define TS_MAXY 3750
