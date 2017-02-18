f1214f82d2688134b67240a2a8bc97b08beec5c0
Samuel Rouse <samuel.rouse@gmail.com>
2017-02-17 19:34:20 -0500

    Add 16-bit colos (5/6/5)

--- colors.h
+++ colors.h
@@ -12,31 +12,31 @@ struct color_s {
 
 struct irColor_s {
   uint32_t irCode;
-//  uint16_t tft;
+  uint16_t tft;
   color_s color;
 };
 
 irColor_s remoteColors [20] = {
-  {0xFF1AE5, {255, 0, 0} }, // Red
-  {0xFF2AD5, {255, 63, 0} }, // Red-Orange (Red 2)
-  {0xFF0AF5, {255, 127, 0} }, // Orange (Red 3)
-  {0xFF38C7, {255, 191, 0} }, // Yellow-Orange (Red 4)
-  {0xFF18E7, {255, 255, 0} }, // Yellow (Red 5)
-  {0xFF9A65, {0, 255, 0} }, // Green
-  {0xFFAA55, {0, 255, 63} }, // Green-Aqua (Green 2)
-  {0xFF8A75, {0, 255, 127} }, // Aqua (Green 3)
-  {0xFFB847, {0, 255, 191} }, // Aqua-Cyan (Green 4)
-  {0xFF9867, {0, 255, 255} }, // Cyan (Green 5)
-  {0xFFA25D, {0, 0, 255} }, // Blue
-  {0xFF926D, {63, 0, 255} }, // Blue-Purple (Blue 2)
-  {0xFFB24D, {127, 0, 255} }, // Purple (Blue 3)
-  {0xFF7887, {191, 0, 255} }, // Purple-Magenta (Blue 4)
-  {0xFF58A7, {255, 0, 255} }, // Magenta (Blue 5)
-  {0xFF22DD, {255, 255, 255} }, // White
-  {0xFF12ED, {255, 191, 191} }, // White 2
-  {0xFF32CD, {255, 191, 191} }, // White 3
-  {0xFFF807, {191, 191, 255} }, // White 4
-  {0xFFD827, {191, 191, 255} } // White 5
+  {0xFF1AE5, 0xF800, {255, 0, 0} }, // Red
+  {0xFF2AD5, 0xFA00, {255, 63, 0} }, // Red-Orange (Red 2)
+  {0xFF0AF5, 0xFBE0, {255, 127, 0} }, // Orange (Red 3)
+  {0xFF38C7, 0xFDE0, {255, 191, 0} }, // Yellow-Orange (Red 4)
+  {0xFF18E7, 0xFFE0, {255, 255, 0} }, // Yellow (Red 5)
+  {0xFF9A65, 0x07E0, {0, 255, 0} }, // Green
+  {0xFFAA55, 0x07E8, {0, 255, 63} }, // Green-Aqua (Green 2)
+  {0xFF8A75, 0x07EF, {0, 255, 127} }, // Aqua (Green 3)
+  {0xFFB847, 0x07F7, {0, 255, 191} }, // Aqua-Cyan (Green 4)
+  {0xFF9867, 0x07FF, {0, 255, 255} }, // Cyan (Green 5)
+  {0xFFA25D, 0x001F, {0, 0, 255} }, // Blue
+  {0xFF926D, 0x401F, {63, 0, 255} }, // Blue-Purple (Blue 2)
+  {0xFFB24D, 0x781F, {127, 0, 255} }, // Purple (Blue 3)
+  {0xFF7887, 0xB81F, {191, 0, 255} }, // Purple-Magenta (Blue 4)
+  {0xFF58A7, 0xF81F, {255, 0, 255} }, // Magenta (Blue 5)
+  {0xFF22DD, 0xFFFF, {255, 255, 255} }, // White
+  {0xFF12ED, 0xFDF7, {255, 191, 191} }, // White 2
+  {0xFF32CD, 0xFDF7, {255, 191, 191} }, // White 3
+  {0xFFF807, 0xBDFF, {191, 191, 255} }, // White 4
+  {0xFFD827, 0xBDFF, {191, 191, 255} } // White 5
 };
 
 #endif
--- esp8266-led-strip-controller.ino
+++ esp8266-led-strip-controller.ino
@@ -15,58 +15,8 @@
   MIT license, all text above must be included in any redistribution
  ****************************************************/
 
-#include <SPI.h>
-#include <Wire.h>      // this is needed even tho we aren't using it
-
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ILI9341.h> // Hardware-specific library
-#include <Adafruit_STMPE610.h>
-
-#ifdef ESP8266
-   #define STMPE_CS 16
-   #define TFT_CS   0
-   #define TFT_DC   15
-   #define SD_CS    2
-#endif
-#ifdef __AVR_ATmega32U4__
-   #define STMPE_CS 6
-   #define TFT_CS   9
-   #define TFT_DC   10
-   #define SD_CS    5
-#endif
-#ifdef ARDUINO_SAMD_FEATHER_M0
-   #define STMPE_CS 6
-   #define TFT_CS   9
-   #define TFT_DC   10
-   #define SD_CS    5
-#endif
-#ifdef TEENSYDUINO
-   #define TFT_DC   10
-   #define TFT_CS   4
-   #define STMPE_CS 3
-   #define SD_CS    8
-#endif
-#ifdef ARDUINO_STM32_FEATHER
-   #define TFT_DC   PB4
-   #define TFT_CS   PA15
-   #define STMPE_CS PC7
-   #define SD_CS    PC5
-#endif
-
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
-Adafruit_STMPE610 ts = Adafruit_STMPE610(STMPE_CS);
-
-#include "ArialRoundedMTBold_36.h"
-
-// Additional UI functions
-#include "GfxUi.h"
-GfxUi ui = GfxUi(&tft);
-
-// This is calibration data for the raw touch data to the screen coordinates
-#define TS_MINX 3800
-#define TS_MAXX 100
-#define TS_MINY 100
-#define TS_MAXY 3750
+#include "tft.h"
+#include "colors.h"
 
 // Size of the color selection boxes and the paintbrush size
 #define BOXSIZE 40
@@ -76,19 +26,19 @@ TS_Point oldpoint, p;
 
 boolean active = true;
 
-void setup(void) {
-  Serial.begin(115200);
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+#define PIXELCOUNT 9
+#define PIXELPIN 4
 
-  delay(10);
-  Serial.println("FeatherWing TFT");
-  if (!ts.begin()) {
-    Serial.println("Couldn't start touchscreen controller");
-    while (1);
-  }
-  Serial.println("Touchscreen started");
-  
-  tft.begin();
-  tft.fillScreen(ILI9341_BLACK);
+// Temp pin setup
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELCOUNT, PIXELPIN, NEO_GRB + NEO_KHZ800);
+
+uint8_t currentPixel = 0;
+
+void paintColors() {
   
   // make the color selection boxes
   // First Row
@@ -106,96 +56,96 @@ void setup(void) {
   tft.fillRect(BOXSIZE*3, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_WHITE);
   tft.fillRect(BOXSIZE*4, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_DARKGREY);
   tft.fillRect(BOXSIZE*5, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_BLACK);
-
- 
-  // select the current color 'red'
-  tft.drawRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-  currentcolor = ILI9341_RED;
 }
 
-void paint() {
+
+void paintSwitch() {
+  // Paint the ON/OFF switch
+  tft.setFont(&ArialRoundedMTBold_36);
+  ui.setTextAlignment(CENTER);
+
+  if (active) {
+    tft.fillRect(0, BOXSIZE*2, BOXSIZE*3, BOXSIZE*2, ILI9341_WHITE);
+    ui.setTextColor(ILI9341_BLACK);
+    ui.drawString(BOXSIZE*1.4, BOXSIZE*3.25, "ON");
+  } else {
+    tft.fillRect(0, BOXSIZE*2, BOXSIZE*3, BOXSIZE*2, ILI9341_BLACK);
+    ui.setTextColor(ILI9341_WHITE);
+    ui.drawString(BOXSIZE*1.4, BOXSIZE*3.25, "OFF");
+  }
+}
+
+void setup(void) {
+  Serial.begin(115200);
+
+  delay(10);
+  Serial.println("FeatherWing TFT");
+  if (!ts.begin()) {
+    Serial.println("Couldn't start touchscreen controller");
+    while (1);
+  }
+  Serial.println("Touchscreen started");
+  
+  tft.begin();
+  tft.fillScreen(ILI9341_BLACK);
+  
+  paintColors();
+  paintSwitch();
+
+  currentcolor = ILI9341_RED;
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void updatePixel() {
+  strip.setPixelColor(currentPixel, currentcolor);
+  currentPixel = (currentPixel + 1) % PIXELCOUNT;
+  strip.show();
+}
+
+void checkSettings() {
+  // Color buttons first
   if (p.y < BOXSIZE*2) {
      oldcolor = currentcolor;
 
      if (p.y < BOXSIZE) {
        if (p.x < BOXSIZE) { 
          currentcolor = ILI9341_RED; 
-         tft.drawRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
        } else if (p.x < BOXSIZE*2) {
          currentcolor = ILI9341_ORANGE;
-         tft.drawRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
        } else if (p.x < BOXSIZE*3) {
          currentcolor = ILI9341_YELLOW;
-         tft.drawRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
        } else if (p.x < BOXSIZE*4) {
          currentcolor = ILI9341_GREENYELLOW;
-         tft.drawRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
        } else if (p.x < BOXSIZE*5) {
          currentcolor = ILI9341_GREEN;
-         tft.drawRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
        } else if (p.x < BOXSIZE*6) {
          currentcolor = ILI9341_CYAN;
-         tft.drawRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
        }
      } else {
        if (p.x < BOXSIZE) { 
          currentcolor = ILI9341_BLUE; 
-         tft.drawRect(0, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_WHITE);
        } else if (p.x < BOXSIZE*2) {
          currentcolor = ILI9341_PURPLE;
-         tft.drawRect(BOXSIZE, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_WHITE);
        } else if (p.x < BOXSIZE*3) {
          currentcolor = ILI9341_MAGENTA;
-         tft.drawRect(BOXSIZE*2, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_WHITE);
        } else if (p.x < BOXSIZE*4) {
          currentcolor = ILI9341_WHITE;
-         tft.drawRect(BOXSIZE*3, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_WHITE);
        } else if (p.x < BOXSIZE*5) {
          currentcolor = ILI9341_DARKGREY;
-         tft.drawRect(BOXSIZE*4, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_WHITE);
        } else if (p.x < BOXSIZE*6) {
          currentcolor = ILI9341_BLACK;
-         tft.drawRect(BOXSIZE*5, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_WHITE);
        }
      }
-
-      if (oldcolor != currentcolor) {
-        if (oldcolor == ILI9341_RED) 
-          tft.fillRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_RED);
-        if (oldcolor == ILI9341_ORANGE) 
-          tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_ORANGE);
-        if (oldcolor == ILI9341_YELLOW) 
-          tft.fillRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, ILI9341_YELLOW);
-        if (oldcolor == ILI9341_GREENYELLOW) 
-          tft.fillRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, ILI9341_GREENYELLOW);
-        if (oldcolor == ILI9341_GREEN) 
-          tft.fillRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, ILI9341_GREEN);
-        if (oldcolor == ILI9341_CYAN) 
-          tft.fillRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, ILI9341_CYAN);
-        if (oldcolor == ILI9341_BLUE) 
-          tft.fillRect(0, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_BLUE);
-        if (oldcolor == ILI9341_PURPLE) 
-          tft.fillRect(BOXSIZE, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_PURPLE);
-        if (oldcolor == ILI9341_MAGENTA) 
-          tft.fillRect(BOXSIZE*2, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_MAGENTA);
-        if (oldcolor == ILI9341_WHITE) 
-          tft.fillRect(BOXSIZE*3, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_WHITE);
-        if (oldcolor == ILI9341_DARKGREY) 
-          tft.fillRect(BOXSIZE*4, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_DARKGREY);
-        if (oldcolor == ILI9341_BLACK) 
-          tft.fillRect(BOXSIZE*5, BOXSIZE, BOXSIZE, BOXSIZE, ILI9341_BLACK);
-     }
+  } else if (p.y < BOXSIZE*5 && p.x < BOXSIZE*3 ) {
+    active = !active;
+    paintSwitch();
   }
 
-  // Paint the Current Color
-  tft.fillRect(BOXSIZE*3, BOXSIZE*2, BOXSIZE*3, BOXSIZE*2, currentcolor);
-
-  // Paint the ON/OFF switch
-  tft.setFont(&ArialRoundedMTBold_36);
-  tft.fillRect(0, BOXSIZE*2, BOXSIZE*3, BOXSIZE*2, ILI9341_YELLOW);
-  ui.setTextColor(ILI9341_BLACK);
-  ui.setTextAlignment(CENTER);
-  ui.drawString(BOXSIZE*1.5, BOXSIZE*3.25, "ON");
+  // Paint the Current Color inset slightly
+  tft.fillRect(BOXSIZE*3 + 2, BOXSIZE*2 + 2, BOXSIZE*3 - 4, BOXSIZE*2 - 4, currentcolor);
+  updatePixel();
 }
 
 boolean samePoint(TS_Point p1, TS_Point p2) {
@@ -203,7 +153,7 @@ boolean samePoint(TS_Point p1, TS_Point p2) {
 }
 
 boolean closePoint(TS_Point p1, TS_Point p2) {
-  return abs(p1.x - p2.x) < 10 && abs(p1.y - p2.y) < 10;
+  return abs(p1.x - p2.x) < 5 && abs(p1.y - p2.y) < 5;
 }
 
 void loop() {
@@ -214,11 +164,14 @@ void loop() {
   p.x = map(p.x, TS_MINX, TS_MAXX, 0, tft.width());
   p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
 
-  if (!closePoint(p, oldpoint)) {
-    Serial.print("X = "); Serial.print(p.x);
+  // ts.touched() solves for buffered input
+  if (ts.touched() && !closePoint(p, oldpoint)) {
+    Serial.print("\nX = "); Serial.print(p.x);
     Serial.print("\tY = "); Serial.print(p.y);
     Serial.print("\tPressure = "); Serial.println(p.z); 
-    paint();
+    checkSettings();
+  } else {
+    Serial.print('.');
   }
 
   // Save last point
